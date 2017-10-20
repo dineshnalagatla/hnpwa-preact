e6ccc24c2a27967c1faee96751e7cd2db466ad5f
Dinesh Nalagatla <dinu.nalagatla@gmail.com>
2017-10-19 23:18:28 -0700

    Initial changes related to hnpwa

---
+++ components/TestComponent.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function createTestApp() {
+    return (<div><h1> Hello World </h1></div>);
+}
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    // configuration
+    context: __dirname,
+    entry: "./hnpwa.js",
+    output: {
+        path: __dirname + "/public/dist",
+        filename: "index.js"
+    },
+    resolve: {
+        extensions: ['.jsx', '.js'],
+		alias: {
+			'react': 'preact-compat',
+			'react-dom': 'preact-compat'
+		}
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.(png|jpg|gif|wav|html|svg|ttf|woff|eot)$/,
+                loader: "file-loader?name=[name].[ext]"
+            },
+            {
+                test: /\.less$/,
+                loader: ExtractTextPlugin.extract({
+                    use: [{
+                        loader: "css-loader"
+                    }, {
+                        loader: "less-loader"
+                    }],
+                    // use style-loader in development
+                    fallback: "style-loader"
+                })
+            },
+            {
+                test: /\.(jsx|js)$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015','react']
+                }
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin("styles.css")
+    ]
+};
